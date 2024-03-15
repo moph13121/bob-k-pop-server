80897457628a748bcda99566cf87ffc8d035eb5c
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-03-15 09:58:15 +0100

    add Product model

---
+++ bob-api/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bob_api.Models
+{
+    public class Product
+    {
+        [Column("id")]
+        public int Id { get; set;}
+
+        [Column("title")]
+        public string Title { get; set;}
+
+        [Column ("description")]
+        public string Description { get; set;}
+
+        [Column("price")]
+        public int Price { get; set;}
+
+        [Column("stock")]
+        public int Stock { get; set;}
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set;} = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
