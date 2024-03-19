281e867c5cfdb62ba502ae283a693483e8c8cc5c
Philip Morud <philip.morud@no.experis.com>
2024-03-19 10:41:25 +0100

    added repository generic for requests

---
+++ bob-api/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace bob_api.Repository
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> Get();
+        Task<T> GetById(object id);
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+    }
+}
---
+++ bob-api/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using bob_api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace bob_api.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DatabaseContext _db;
+        private DbSet<T> _table = null;
+
+        public Repository(DatabaseContext dataContext)
+        {
+            _db = dataContext;
+            _table = _db.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> Get()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<T> GetById(object id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            await _table.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+        public async Task<T> Update(T entity)
+        {
+            _table.Attach(entity);
+            _db.Entry(entity).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Deleted;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
